789da9e70dfe07cb400d7557a9152ee6c88a7932
Chronix <phoniexpaul88@gmail.com>
2025-07-09 21:18:17 +0530

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+git clone --depth=1 https://github.com/ChroniX-AOSP/platform_vendor_xiaomi_violet.git vendor/xiaomi/violet
+git clone https://github.com/ChroniX-AOSP/platform_kernel_xiaomi_violet.git -b Surukuu-1.0.1-LTS kernel/xiaomi/violet
+git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
+git clone https://github.com/selfmusing/vendor_bcr.git vendor/bcr
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
+git clone https://github.com/zenin1504/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
