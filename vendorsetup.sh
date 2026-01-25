b916010cd339ba5d5a86b4d248d64ed885c1fa40
Chronix <phoniexpaul88@gmail.com>
2026-01-25 13:49:10 +0530

    Switch to Xcalibur

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,5 @@
 git clone --depth=1 https://github.com/kibria5/android_vendor_xiaomi_violet.git vendor/xiaomi/violet
-git clone https://github.com/kibria5/android_kernel_xiaomi_violet.git -b 14 kernel/xiaomi/violet
+git clone https://github.com/Joker-V2/kernel_xiaomi_violet.git -b fourteen kernel/xiaomi/violet
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
-git clone https://github.com/selfmusing/vendor_bcr.git vendor/bcr
 git clone --depth=1 https://github.com/selfmusing/meme_cam.git vendor/MiuiCamera
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
