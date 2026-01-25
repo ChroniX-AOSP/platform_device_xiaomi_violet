5708fd80dfa30da1d21f23d99260d5ec035d92cd
ChroniX-AOSP <phoniexpaul88@gmail.com>
2026-01-25 10:34:11 +0530

    Rising !

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,14 @@
 #
 # Copyright (C) 2018-2020 The LineageOS Project
-# Copyright (C) 2020 The AfterLife Project
+# Copyright (C) 2020 The RisingOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/afterlife_violet.mk
+    $(LOCAL_DIR)/lineage_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    afterlife_violet-eng \
-    afterlife_violet-userdebug \
-    afterlife_violet-user
+    lineage_violet-eng \
+    lineage_violet-userdebug \
+    lineage_violet-user
--- lineage_violet.mk
+++ lineage_violet.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2018-2020 The LineageOS Project
-# Copyright (C) 2020 The AfterLife Project
+# Copyright (C) 2020 The lineage Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,13 +8,16 @@
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common AfterlifeOS stuff.
-$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
+# Inherit some common lineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# AfterLife flags
-AFTERLIFE_MAINTAINER := chronix
-AFTERLIFE_GAPPS := true
+# RisingOS
+WITH_GMS := true
+TARGET_ENABLE_BLUR := false
+PRODUCT_NO_CAMERA := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
 
 # Misc
 TARGET_FACE_UNLOCK_SUPPORTED := true
@@ -23,13 +26,15 @@ USE_PIXEL_CHARGING := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := afterlife_violet
+PRODUCT_NAME := lineage_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="violet"
+    PRODUCT_NAME="violet" \
+    RISING_CHIPSET="Snapdragon 675 " \
+    RISING_MAINTAINER="Chronix魚"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
