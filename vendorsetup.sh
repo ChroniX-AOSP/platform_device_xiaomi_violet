98f193aa977d76e57c5d245b7375d45dbf22b203
Chronix <phoniexpaul88@gmail.com>
2026-02-01 14:06:50 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,11 +4,11 @@ echo "Cloning The Vendor"
 git clone --depth=1 https://github.com/kibria5/android_vendor_xiaomi_violet.git vendor/xiaomi/violet
 
 echo "Cloning The Kernel"
-git clone https://github.com/kibria5/android_kernel_xiaomi_violet.git -b 14 kernel/xiaomi/violet
+git clone --depth=1 https://github.com/kibria5/android_kernel_xiaomi_violet.git -b 14 kernel/xiaomi/violet 
 
 echo "Cloning Some More Stuff."
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
-git clone --depth=1 https://github.com/selfmusing/meme_cam.git vendor/MiuiCamera
+git clone --depth=1 https://gitlab.com/Joker-V2/android_vendor_MiuiCamera vendor/MiuiCamera
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
 
 echo "Setup Complete"
